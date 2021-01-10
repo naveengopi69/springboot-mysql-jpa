b12617338f18537f8bb58a0cbd5b54512ad1af7e
Gopiraja <Gopiraja@Gopi>
2021-01-10 18:11:53 +0530

    DELETE

--- src/main/java/com/bezkoder/spring/datajpa/repository/ExpenseRepository.java
+++ src/main/java/com/bezkoder/spring/datajpa/repository/ExpenseRepository.java
@@ -1,9 +1,16 @@
 package com.bezkoder.spring.datajpa.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.bezkoder.spring.datajpa.model.Expense;
 
 public interface ExpenseRepository extends JpaRepository<Expense, Integer> {
+	//List<Expense> findByUserId(int userId);
+
+	//List<Expense> findByUserIdAndExpenseTypeAndDateBetween(int userId, String expenseType, String fromDate,
+		//	String toDate);
+
 
 }
