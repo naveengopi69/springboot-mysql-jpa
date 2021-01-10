95caf588c622a478f83df563cdea99acd3c5a5a0
Gopiraja <Gopiraja@Gopi>
2021-01-10 20:11:37 +0530

    Added changes to delete expense function...

--- src/main/java/com/bezkoder/spring/datajpa/controller/ExpenseControler.java
+++ src/main/java/com/bezkoder/spring/datajpa/controller/ExpenseControler.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -13,7 +14,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.bezkoder.spring.datajpa.model.Expense;
-import com.bezkoder.spring.datajpa.model.Tutorial;
 import com.bezkoder.spring.datajpa.repository.ExpenseRepository;
 
 @RestController
@@ -51,6 +51,15 @@ public class ExpenseControler {
 			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
 		}
 		//abcdef
+	}
+	@DeleteMapping("/expense/{id}")
+	public ResponseEntity<HttpStatus> deleteExpense(@PathVariable("expenseId") int id) {
+		try {
+			expenseRepository.deleteById(id);
+			return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+		} catch (Exception e) {
+			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
 	}
 }
 
